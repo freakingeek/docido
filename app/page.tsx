2f8611d71aa2caca38e079af1cbed304e3479f35
EhsaNik <ehsanik19@gmail.com>
2024-11-26 17:45:26 +0330

    chore: add section 'under study' in page-component

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,11 @@
 'use client'
-import '../app/globals.css'
 import Link from 'next/link'
 import Image from 'next/image'
 import 'swiper/css/pagination'
+import 'swiper/css/navigation'
 import Header from '@/components/Header'
-import { Pagination } from 'swiper/modules'
 import { Swiper, SwiperSlide } from 'swiper/react'
+import { Pagination, Navigation } from 'swiper/modules'
 import {
   BookOpen,
   Book,
@@ -15,6 +15,7 @@ import {
   File,
   Settings,
   Search,
+  PlayCircle,
   ChevronDown,
   ShoppingBag,
   User,
@@ -95,6 +96,40 @@ export default function HomePage() {
       id: 9,
       img: 'https://fastly.picsum.photos/id/788/220/330.jpg?hmac=uCojuT1AtjC0Kv1p-s3t1gGGscZ1wKc5bpzbuyAXFSg',
       link: '/',
+    },
+  ]
+  const posts = [
+    {
+      id: 0,
+      img: 'https://fastly.picsum.photos/id/683/103/155.jpg?hmac=q4E3Pi7QEn3w1mxTp8afRhOjmA08DdKDivaXnYBKqKs',
+      type: 'کتاب صوتی',
+      link: '/',
+      title: 'داستان دورگه: سحرگاه',
+      discription: 'پدیدآور: احمد احمدی - ناشر:  آوانامه',
+    },
+    {
+      id: 1,
+      img: 'https://fastly.picsum.photos/id/767/103/155.jpg?hmac=IgQ3ugydfY6_0ATkmTNU52vGtxHN11OCM6SXHcU0d_Q',
+      type: 'کتاب صوتی',
+      link: '/',
+      title: 'داستان دورگه: سحرگاه',
+      discription: 'پدیدآور: احمد احمدی - ناشر:  آوانامه',
+    },
+    {
+      id: 2,
+      img: 'https://fastly.picsum.photos/id/721/103/155.jpg?hmac=rJ07zuX1ooY6lWhtXh-aKKjLj7L31Bq5Vak71JQSaGI',
+      type: 'کتاب صوتی',
+      link: '/',
+      title: 'داستان دورگه: سحرگاه',
+      discription: 'پدیدآور: احمد احمدی - ناشر:  آوانامه',
+    },
+    {
+      id: 3,
+      img: 'https://fastly.picsum.photos/id/253/103/155.jpg?hmac=6hhXs8nxmYJ1QozeXjjtdoKyqytoNX8lIqAAxpqcnE4',
+      type: 'کتاب صوتی',
+      link: '/',
+      title: 'داستان دورگه: سحرگاه',
+      discription: 'پدیدآور: احمد احمدی - ناشر:  آوانامه',
     },
   ]
 
@@ -182,6 +217,54 @@ export default function HomePage() {
               </Link>
             </li>
           </ul>
+        </div>
+
+        {/* Section under study */}
+        <div className="flex flex-col justify-center cursor-pointer w-[1656px] -mx-[60px] p-11 h-80 bg-ashwood items-start rounded-[50px] mt-9">
+          <span className="flex px-4 text-content-high text-xl font-extrabold pb-3">
+            درحال مطالعه
+          </span>
+          <Swiper
+            modules={[Navigation]}
+            navigation={true}
+            slidesPerView={3}
+            spaceBetween={16}
+            className="w-full px-4 text-content-high"
+          >
+            {posts.map((post) => (
+              <SwiperSlide
+                key={post.id}
+                className="flex w-[544px] h-48 gap-x-4 rounded-3xl bg-lightAlb hover:bg-alabaster transition duration-300"
+              >
+                <div className="flex flex-col justify-center px-4">
+                  <img src={post.img} className="rounded-3xl" />
+                </div>
+
+                <div className="flex flex-col justify-start mt-4">
+                  <span className="font-semibold text-xl leading-9">{post.title}</span>
+                  <span className="font-normal text-base leading-7">{post.discription}</span>
+                  <Link
+                    href={post.link}
+                    className="flex justify-center items-center w-[107px] h-9 text-content-low bg-WhiteCoffee rounded-xl gap-x-2 mt-1"
+                  >
+                    <Headphones size={20} />
+                    <span className="text-sm font-medium">{post.type}</span>
+                  </Link>
+                  <div id="progress" className="flex flex-col h-0.5 w-80 bg-WhiteCoffee mt-3">
+                    <div className="flex justify-between mt-4">
+                      <span className="text-content-high">
+                        <span className="text-lg font-extrabold">3%</span>
+                        <span className="text-sm font-normal">شنیده اید</span>
+                      </span>
+                      <button className="text-content-low">
+                        <PlayCircle size={32} />
+                      </button>
+                    </div>
+                  </div>
+                </div>
+              </SwiperSlide>
+            ))}
+          </Swiper>
         </div>
 
         <div>
