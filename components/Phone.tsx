7b6886998ed9309650f87e5acff5cd4cc51e4e95
EhsaNik <ehsanik19@gmail.com>
2024-11-27 21:45:50 +0330

    chore: add slider in branch "phone"

--- components/Phone.tsx
+++ components/Phone.tsx
@@ -1,6 +1,10 @@
 import classNames from 'classnames'
 import Link from 'next/link'
+import Image from 'next/image'
 import React from 'react'
+import 'swiper/css/pagination'
+import 'swiper/css/navigation'
+import { Swiper, SwiperSlide } from 'swiper/react'
 import Logo from './Logo'
 import { ShoppingBag, Settings, Search } from 'react-feather'
 
@@ -9,6 +13,30 @@ type PhoneProps = {
 }
 
 export default function Phone({ className }: PhoneProps) {
+  const sliders = [
+    {
+      id: 0,
+      link: '/',
+      img: 'https://fastly.picsum.photos/id/161/1800/400.jpg?hmac=kbzNpgQNcp-oZK6IpMevfqr4s7gRv2XKdgm8dpRlSN0',
+      title: 'جای شما و کتابخانه ما خالی....',
+      description: 'بیش از 2.000.00 کتاب دیجیتال و 100.000 کتاب صوتی',
+    },
+    {
+      id: 1,
+      link: '/',
+      img: 'https://fastly.picsum.photos/id/112/1800/400.jpg?hmac=Sqldq12aR-NiAByftsl24oYae-GghQtZx_S0ENOuKIE',
+      title: 'جای شما و کتابخانه ما خالی....',
+      description: 'بیش از 2.000.00 کتاب دیجیتال و 100.000 کتاب صوتی',
+    },
+    {
+      id: 2,
+      link: '/',
+      img: 'https://fastly.picsum.photos/id/490/1800/400.jpg?hmac=biLv_lkFMxKWCxK3kmdUN-0s9gRozTH1CU8AwpuGLT8',
+      title: 'جای شما و کتابخانه ما خالی....',
+      description: 'بیش از 2.000.00 کتاب دیجیتال و 100.000 کتاب صوتی',
+    },
+  ]
+
   return (
     <div className={classNames('max-w-sm mx-auto w-full bg-alabaster p-4', className)}>
       <header className="flex flex-col">
@@ -45,6 +73,30 @@ export default function Phone({ className }: PhoneProps) {
           />
         </div>
       </header>
+      <main>
+        {/* Banner slider */}
+        <Swiper spaceBetween={16} className="h-28 mt-5">
+          {sliders.map((slider) => (
+            <SwiperSlide key={slider.id} className="w-full h-full relative">
+              <div>
+                <Image
+                  fill
+                  alt={slider.link}
+                  sizes="1536px"
+                  className="rounded-2xl"
+                  src={slider.img}
+                />
+              </div>
+              <div className="absolute top-11 left-5 flex flex-col">
+                <span className="text-white text-xs font-black leading-5">{slider.title}</span>
+                {slider.title && (
+                  <span className="text-white text-[7px] font-normal">{slider.description}</span>
+                )}
+              </div>
+            </SwiperSlide>
+          ))}
+        </Swiper>
+      </main>
     </div>
   )
 }
